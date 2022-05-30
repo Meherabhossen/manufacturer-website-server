e5164165cfd6730f5ba2962712b5c9a086beca6b
Meherabhossen <meherabhossen45@gmail.com>
2022-05-30 22:45:50 +0600

    new add

--- index.js
+++ index.js
@@ -62,21 +62,21 @@ async function run() {
             const isAdmin = user.role === 'admin';
             res.send({ admin: isAdmin })
         })
-        app.put('/user/admin/:email', verifyAdmin, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester });
-            if (requesterAccount.role === 'admin') {
+            // const requester = req.decoded.email;
+            // const requesterAccount = await userCollection.findOne({ email: requester });
+            // if (requesterAccount.role === 'admin') {
                 const filter = { email: email };
                 const updateDoc = {
                     $set: { role: 'admin' },
                 };
                 const result = await userCollection.updateOne(filter, updateDoc);
                 res.send(result);
-            }
-            else {
-                res.status(403).send({ message: 'Forbidden' });
-            }
+            // }
+            // else {
+            //     res.status(403).send({ message: 'Forbidden' });
+            // }
         })
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -136,3 +136,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Manufacture app listening on port ${port}`)
 })
+
